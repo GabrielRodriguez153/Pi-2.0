12392e46c3e57df9ed472bd5e22160d76c812da4
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2024-05-25 22:49:26 -0300

    Merge componentes
    Mesclando componentes e adicionando forEach de apartamentos

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var $grid = $(".grid").isotope({
+    itemSelector: ".hotel-card",
+    layoutMode: "masonry",
+    masonry: {
+      columnWidth: ".hotel-card",
+    },
+  });
+
+  // Toggle dropdown content and relayout Isotope
+  var dropdowns = document.querySelectorAll(".dropdown-btn");
+  dropdowns.forEach(function (dropdown) {
+    dropdown.addEventListener("click", function () {
+      var card = this.closest(".hotel-card");
+      var dropdownContent = card.querySelector(".dropdown-content");
+      var icon = this.querySelector("i");
+
+      if (dropdownContent.classList.contains("expanded")) {
+        dropdownContent.classList.remove("expanded");
+        card.classList.remove("expanded");
+        icon.classList.remove("fa-chevron-up");
+        icon.classList.add("fa-chevron-down");
+      } else {
+        dropdownContent.classList.add("expanded");
+        card.classList.add("expanded");
+        icon.classList.remove("fa-chevron-down");
+        icon.classList.add("fa-chevron-up");
+      }
+
+      $grid.isotope("layout");
+    });
+  });
+});
+
+
